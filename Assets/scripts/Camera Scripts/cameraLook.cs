8638b891b2109ef5357f2dfe3ea02a0a19b066e2
Angela <agv7@njit.edu>
2018-02-23 19:09:29 -0500

    Basic Grid Snapping
    Debug grid drawn, as well as test box spawing to test snapping to nearest hole.

--- Assets/scripts/Camera Scripts/cameraLook.cs
+++ Assets/scripts/Camera Scripts/cameraLook.cs
@@ -11,8 +11,10 @@ public class cameraLook : MonoBehaviour {
 	private Vector3 currentRotation;
 	private bool left, right = false;
 
+	private gridLayout grid;
 
 	void Start () {
+		grid = FindObjectOfType<gridLayout> ();
 		currentRotation = Vector3.zero;
 	}
 
@@ -23,7 +25,10 @@ public class cameraLook : MonoBehaviour {
 			if (Physics.Raycast (ray, out hit)) {
 				if (hit.transform.name == "BreadBoard") {
 					hit.transform.gameObject.GetComponent<selectGlow> ().zoomedIn = true;
-					StartCoroutine (zoomIn());
+					hit.transform.gameObject.GetComponent<Collider> ().enabled = false;
+					StartCoroutine (zoomIn ());
+				} else {
+					PlaceCubeNear (hit.point);
 				}
 			}
 		}
@@ -33,7 +38,6 @@ public class cameraLook : MonoBehaviour {
 		currentRotation.x = Input.GetAxis ("Vertical");
 		currentRotation.y = Input.GetAxis ("Horizontal");
 
-
 		if (Input.GetKey (KeyCode.A)) {
 				transform.Rotate (currentRotation * 75f * Time.deltaTime);
 		}
@@ -41,14 +45,21 @@ public class cameraLook : MonoBehaviour {
 			currentRotation.x = -1 * currentRotation.x;
 			transform.Rotate (new Vector3 (0.25f, transform.rotation.y, transform.rotation.z));
 		}
-		print (transform.position);
 	}
 
 	IEnumerator zoomIn(){
 		while (transform.position != newPosition) {
 			transform.position = Vector3.MoveTowards (transform.position, newPosition, 10f * Time.deltaTime);
-			transform.eulerAngles = Vector3.RotateTowards (transform.eulerAngles, newRotation, 10f, 10f);
+			if (transform.eulerAngles != newRotation)
+				transform.eulerAngles = Vector3.RotateTowards (transform.eulerAngles, newRotation, 10f, 10f);
 			yield return null;
 		}
+		print (transform.position == newPosition);
+	}
+	void PlaceCubeNear(Vector3 clickPoint){
+		Vector3 final = grid.GetNearestPointOnGrid (clickPoint);
+		GameObject cube = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		cube.transform.localScale = cube.transform.localScale * 0.05f;
+		cube.transform.position = final;
 	}
 }
---
+++ Assets/scripts/Component Scripts/gridLayout.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridLayout : MonoBehaviour
+{
+	[SerializeField]
+	private float size = 1f;
+	public Renderer rend;
+	public int holeCount, rowCount = 0;
+	private Vector3[] gridPositions;
+
+	void Start(){
+		
+	}
+
+	public Vector3 GetNearestPointOnGrid(Vector3 position)
+	{
+		Vector3 result = Vector3.zero;
+		for (int i = 0; i < gridPositions.Length; i++) {
+			print (gridPositions [i]);
+			if (i == 0) {
+				result = gridPositions [i];
+			}
+			else if (Vector3.Distance (position, gridPositions [i]) < Vector3.Distance (position, result)) {
+				result = gridPositions [i];
+			}
+		}
+
+		return result;
+	}
+
+	private void OnDrawGizmos()
+	{
+		BoxCollider b = GetComponent<BoxCollider> ();
+
+		Gizmos.color = Color.cyan;
+		Vector3 start = transform.TransformPoint (b.center - new Vector3 (b.size.x - 0.017f, -b.size.y - 0.01f, -b.size.z) * 0.5f);
+		Vector3 newPos = start;
+		float rowCountTemp = rowCount;
+		int k = 0;
+		gridPositions = new Vector3[holeCount * rowCount];
+
+		for (float x = 0; x < holeCount; x += size)
+		{
+			for (float z = 0; z < rowCount; z += size)
+			{
+				if (rowCountTemp == 8) {
+					newPos = new Vector3 (start.x, newPos.y, newPos.z + 0.085f);
+					rowCountTemp = 0;
+				} else {
+					newPos = new Vector3 (newPos.x + 0.07f, newPos.y, newPos.z);
+				}
+				gridPositions [k] = newPos;
+				k++;
+				rowCountTemp++;
+				Gizmos.DrawSphere(newPos, 0.02f);
+			}
+		}
+	}
+
+}
