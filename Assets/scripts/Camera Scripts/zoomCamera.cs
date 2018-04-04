4a0695aa199c796a6230f45cda5c070737788544
Angela <agv7@njit.edu>
2018-04-04 16:40:26 -0400

    Added a magnifying class effect for the circuit diagram

---
+++ Assets/scripts/Camera Scripts/zoomCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoomCamera : MonoBehaviour {
+
+	public Camera sCam;
+	private Vector3 sCamOrigin;
+
+	void Start () {
+		sCamOrigin = sCam.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseOver(){
+		sCam.gameObject.SetActive (true);
+		sCam.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
+
+	void OnMouseExit(){
+		sCam.transform.position = sCamOrigin;
+		sCam.gameObject.SetActive (false);
+	}
+}
