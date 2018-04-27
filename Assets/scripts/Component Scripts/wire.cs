6acc83c01158a9e3cbb484e7cf824618f30f5593
Kevin Butryn <kevinbutryn@gmail.com>
2018-04-27 16:07:19 -0400

    added wire file

---
+++ Assets/scripts/Component Scripts/wire.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wire : circuitComponent {
+
+
+	//Empty constructor
+	public wire()
+	{
+		
+		componentType = 99;
+	}
+
+	//Full constructor    
+	public wire( componentNode[] initInputNode, componentNode[] initOutputNode, bool initIsLocked) : base(initInputNode, initOutputNode, initIsLocked)
+	{
+		componentType = 99;
+	}
+		
+
+	//method to perform component function
+	public new bool doComponentLogic(double circuitVoltage, double circuitCurrent)
+	{
+		//default part works fine
+		return true;
+	}
+
+	public new bool doComponentLogic()
+	{
+		//default part works fine
+		return true;
+	}
+}
